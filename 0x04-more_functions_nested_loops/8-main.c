caa56679a9bce856d21cc89c17f7154090d0ac10
semira seid <sseid6957@gmail.com>
2023-03-24 05:10:45 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_squre(2);
+    print_squre(10);
+    print_squre(0);
+    return (0);
+}
